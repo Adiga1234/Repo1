34133f35aac6cd2a47ee4124b62cd566b28b1a61
sou <adiga.soumya@gmail.com>
2021-06-10 22:11:10 +0530

    My first commit

---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            public int accnum;
+           public  double balance = 0;
+
+                  }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
